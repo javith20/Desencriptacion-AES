c2895e01d207203275c6a4752b3bef22e51b3cde
javith20 <43230203+javith20@users.noreply.github.com>
2019-09-27 20:04:44 -0600

    Proyecto Final
    Todo funciona, pruebas realizadas

--- Desencriptacion-AES-develop/src/algorithm_logic/Combination.java
+++ Desencriptacion-AES-develop/src/algorithm_logic/Combination.java
@@ -1,13 +1,14 @@
 package algorithm_logic;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Combination {
 
     private double success_percentage = 0.0d;
     private ArrayList<String> group_letters = new ArrayList<String>();
     private ArrayList<String> group_numbers = new ArrayList<String>();
-
+    private String missing_key ="";
     public Combination() {
     }
     
@@ -28,10 +29,12 @@ public class Combination {
     public Combination obtain_common_elements(Combination second_combination) {
     	Combination temp_subgroup = new Combination(this.getGroup_letters(), this.getGroup_numbers());
     	temp_subgroup.getGroup_letters().retainAll(second_combination.getGroup_letters());
+    	temp_subgroup.getGroup_numbers().retainAll(second_combination.getGroup_numbers());
+    	temp_subgroup.setGroup_letters((ArrayList<String>)group_letters.stream().distinct().collect(Collectors.toList()));
+    	temp_subgroup.setGroup_numbers((ArrayList<String>)group_numbers.stream().distinct().collect(Collectors.toList()));
     	return temp_subgroup;
     }
 
-    //Carga los valores iniciales de numeros y letras
     public void init_values() {
         char value = 97;
         while (value != 123) {
@@ -42,33 +45,19 @@ public class Combination {
             group_numbers.add(String.valueOf(value++));
         }
     }
-
+    
     public Combination(ArrayList<String> _group_letters, ArrayList<String> _group_numbers) {
         this.group_letters = _group_letters;
         this.group_numbers = _group_numbers;
     }
-
-    public void fill_subgroup( Combination original_structure, Combination current_structure) {
-        Random random_index = new Random();
-        for (int letter_index = 0; letter_index < original_structure.getGroup_letters().size(); letter_index++) {
-            int rand_letter_index = random_index.nextInt(original_structure.getGroup_letters().size());
-            current_structure.group_letters.add(original_structure.group_letters.get(rand_letter_index));
-        }
-        for (int number_index = 0; number_index < original_structure.getGroup_numbers().size(); number_index++) {
-            int rand_number_index = random_index.nextInt(original_structure.getGroup_numbers().size());
-            current_structure.group_numbers.add(original_structure.group_numbers.get(rand_number_index));
-            //current_structure.group_numbers[number_index] = original_structure.group_numbers[rand_number_index];
-        }
-    }
-
-    //Tantea posibles soluciones con un porcentaje de intentos, guarda la cantidad de aciertos??
+    
     public void try_combinations(double _percentage) {
         final String txt_encrypted = "xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
         final String[] txt_key_incomplete = {"29dh120", "dk1", "3"};
         Random random_index = new Random();
-        int trys = (int) (this.getGroup_letters().size() * _percentage);
+        int trys = 0;
         int hits = 0;
-        while (trys > 0) {
+        while (trys < (int) (this.getGroup_letters().size() * _percentage)) {
             int letter_rand_index = random_index.nextInt(this.getGroup_letters().size() - 1);
             int numbers_rand_index = random_index.nextInt(this.getGroup_numbers().size() - 1);
             String posible_key = txt_key_incomplete[0] + this.getGroup_letters().get(letter_rand_index);
@@ -76,56 +65,59 @@ public class Combination {
             if (AES.decrypt(txt_encrypted, posible_key) != null) {
                 hits++;
             }
-            trys--;
+            trys++;
 
         }
+       
         this.success_percentage = hits;
     }
     public boolean validate_string(String _string){
         
-        //for(int index =0 ;index<_string.length();index++)
-          //  if(_string[0]<31)
-          return false;
+        for(int index =0 ;index<_string.length();index++) {
+        	int letter =(int)_string.charAt(index);
+        	 if(letter<32)
+        		 return false;
+        }
+          
+          return true;
     }
     public String brute_force() {
         final String txt_encrypted = "xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
         final String[] txt_key_incomplete = {"29dh120", "dk1", "3"};
-        int trys=0;
+        
         for (String letter : this.group_letters) {
             for (String number : this.group_numbers) {
-                trys++;
                 String posible_key = txt_key_incomplete[0] + letter + txt_key_incomplete[1] + number + txt_key_incomplete[2];
                 String decry = AES.decrypt(txt_encrypted, posible_key);
-                System.out.println(letter+number);
                 if (decry != null) {
-                    System.out.println(trys);
-                    return decry;
+                	if(validate_string(decry)) {
+                		this.missing_key= letter+number;
+                		return "\nRespuesta final: "+decry+"\nCaracteres faltantes: "+missing_key;
+                	}
                 }
             }
         }
-        System.out.println(trys);
+       
         return null;
     }
 
-    //toma los valores de la combinacion actual(padre) y genera un hijo en base a este, 
-    //_percentage es el porcionto de valores a tomar
     public Combination create_new_combination(double _percentage) {
         ArrayList<String> aux_group_letters = new ArrayList<>();
         ArrayList<String> aux_group_numbers = new ArrayList<>();
-        int posicion = 0;
+        int position = 0;
         Random random_index = new Random();
         double total = (this.getGroup_letters().size()) * _percentage;
 
         while (aux_group_letters.size() <= total) {
-            posicion = random_index.nextInt(this.getGroup_letters().size());
-            aux_group_letters.add(this.getGroup_letters().get(posicion));
+        	position = random_index.nextInt(this.getGroup_letters().size());
+            aux_group_letters.add(this.getGroup_letters().get(position));
            
         }
         total = (this.getGroup_numbers().size()) * _percentage;
 
         while (aux_group_numbers.size() <= total) {
-            posicion = random_index.nextInt(this.getGroup_numbers().size());
-            aux_group_numbers.add(this.getGroup_numbers().get(posicion));
+        	position = random_index.nextInt(this.getGroup_numbers().size());
+            aux_group_numbers.add(this.getGroup_numbers().get(position));
             
         }
         
@@ -134,12 +126,12 @@ public class Combination {
 
     public void print_structure( ) {
         System.out.println(" ");
-        for (int i = 0; i < this.group_letters.size(); i++) {
-            System.out.print(this.group_letters.get(i));
+        for (int index = 0; index < this.group_letters.size(); index++) {
+            System.out.print(this.group_letters.get(index));
         }
         System.out.print(" ");
-        for (int i = 0; i < this.group_numbers.size(); i++) {
-            System.out.print(this.group_numbers.get(i));
+        for (int index = 0; index < this.group_numbers.size(); index++) {
+            System.out.print(this.group_numbers.get(index));
         }
 
     }
@@ -157,9 +149,14 @@ public class Combination {
     }
 
     public void setGroup_letters(ArrayList<String> group_letters) {
-        this.group_letters = group_letters;
+        this.group_letters =  group_letters;
     }
 
+    public String getMissing_keys() {
+        return this.missing_key;
+    }
+
+    
     public ArrayList<String> getGroup_numbers() {
         return group_numbers;
     }
--- Desencriptacion-AES-develop/src/algorithm_logic/main.java
+++ Desencriptacion-AES-develop/src/algorithm_logic/main.java
@@ -5,16 +5,47 @@ public class main {
 	public static void main(String[] args) {												
 			Combination original_structure = new Combination();
 			original_structure.init_values();
-			Combination first_subgroup = original_structure.create_new_combination(0.5);
-			Combination second_subgroup = original_structure.create_new_combination(0.5);
-			first_subgroup.try_combinations(0.50);
-			second_subgroup.try_combinations(0.50);
-			first_subgroup.print_structure();
-			second_subgroup.print_structure();
+			int total_tests=0;
 			
-			System.out.println("\n"+obtain_highest_success_rate(first_subgroup, second_subgroup).getSuccess_percentage());
+			String final_answer =find_son(original_structure,0).brute_force();
+			while(null==final_answer) {
+				final_answer=find_son(original_structure,0).brute_force();
+				
+				total_tests++;
+				}
+			System.out.println(final_answer);
+			
+			System.out.println("\nTotal de pruebas fallidas: "+total_tests);
+			
 			
 
+	}
+	public static Combination find_son(Combination _father,int trys) {
+		
+		double initial_test_percentage = 0.5;
+		double subsequent_tests_percentage = 0.3;
+		if(_father.amount_is_satisfactory(8)||trys>20) {
+			return _father;
+		}
+		
+		
+		Combination first_subgroup = _father.create_new_combination(initial_test_percentage);
+		Combination second_subgroup = _father.create_new_combination(initial_test_percentage);
+		first_subgroup.try_combinations(subsequent_tests_percentage);
+		second_subgroup.try_combinations(subsequent_tests_percentage);
+		
+		if(first_subgroup.getSuccess_percentage()!=0&&first_subgroup.getSuccess_percentage()!=0) {
+			return find_son(first_subgroup.obtain_common_elements(second_subgroup),0);
+		}
+		if(obtain_highest_success_rate(first_subgroup, second_subgroup).getSuccess_percentage()==0) {
+			
+			return find_son(_father,++trys);
+		}
+		else {
+			return find_son(obtain_highest_success_rate(first_subgroup, second_subgroup),++trys);
+			
+		}
+			
 	}
 	public static Combination obtain_highest_success_rate(Combination combination1, Combination combination2) {
 		boolean first_is_higher = combination1.getSuccess_percentage()>=combination2.getSuccess_percentage();
